55745a0dc38a66b7d19dfe4413e7cce8dbc0c472
James Billy <james@epicguru.co.uk>
2017-10-18 21:57:54 +0200

    Implemented basic form of mining drill.

---
+++ Assets/MiningDrill.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MiningDrill : NetworkBehaviour
+{
+    [SyncVar]
+    public bool Active;
+    public Animator Animator;
+
+    private ItemPickup Pickup;
+
+    public void Start()
+    {
+        Pickup = GetComponent<ItemPickup>();
+    }
+
+    public void Update()
+    {
+        UpdateActiveState();
+        UpdateAnimation();
+    }
+
+    public void UpdateActiveState()
+    {
+        if (Pickup.MouseOver && InputManager.Active)
+        {
+            ActionHUD.DisplayAction("Press " + InputManager.GetInput("Interact") + " to " + (Active ? "turn off" : "turn on") + " mining drill.");
+            if (InputManager.InputDown("Interact"))
+            {
+                // TODO Network!
+                Active = !Active;
+            }
+        }
+    }
+
+    public void UpdateAnimation()
+    {
+        Animator.SetBool("Active", Active);
+    }
+}
