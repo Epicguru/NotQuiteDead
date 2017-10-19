a30333d987adc58ac9908ca382155fb8dd48496c
James Billy <james@epicguru.co.uk>
2017-10-19 22:19:22 +0200

    Added grenade effect, damage and particles. TODO make shake.

---
+++ Assets/Scripts/Camera/CameraShake.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    // EDIT - Its late and (most of) this is not my code, too tired to think right now. Thanks to https://gist.github.com/jakevsrobots/7649147.
+
+    public Camera Camera;
+
+    public float ShakeStrength = 5;
+    public float Shake = 1;
+
+    Vector3 originalPosition;
+
+    public void LateUpdate()
+    {
+        originalPosition = Camera.transform.position;
+        Camera.transform.localPosition = originalPosition + (Random.insideUnitSphere * Shake);
+
+        Shake = Mathf.MoveTowards(Shake, 0, Time.deltaTime * ShakeStrength);
+
+        if (Shake == 0)
+        {
+            Camera.transform.localPosition = originalPosition;
+        }
+    }
+
+    public void ShakeCamera(float strength)
+    {
+        ShakeStrength = strength;
+        Shake = ShakeStrength;
+    }
+}
--- Assets/Scripts/Effects/Explosion.cs
+++ Assets/Scripts/Effects/Explosion.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class Explosion : MonoBehaviour {
 
@@ -11,6 +12,12 @@ public class Explosion : MonoBehaviour {
         GetComponent<Animator>().SetTrigger("Explode");
         Particle.time = 0;
         Particle.Play(true);
+
+        if (NetworkServer.active)
+        {
+            // TODO FIXME!
+            ExplosionDamage(transform.position, 10, "Explosion", 20, 300, AnimationCurve.Linear(0, 1, 1, 0));
+        }
     }
 
 	public void ExplosionEnded()
@@ -22,5 +29,37 @@ public class Explosion : MonoBehaviour {
     public static void Spawn(Vector2 position)
     {
         Player.Local.NetUtils.CmdSpawnExplosion(position);
+    }
+
+    private static List<Health> HitObjects = new List<Health>();
+    public void ExplosionDamage(Vector2 point, float radius, string killer, float minDamage, float maxDamage, AnimationCurve Curve)
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(point, radius);
+
+        foreach(Collider2D c in colliders)
+        {
+            Health h = c.GetComponentInParent<Health>();
+            if (h == null)
+                continue;
+            if (!h.CanHit)
+                continue;
+            if (h.CannotHit.Contains(c))
+                continue;
+            if (HitObjects.Contains(h))
+                continue;
+            // TODO team damage?
+
+            float distanceFromCenter = Vector2.Distance(point, c.transform.position);
+
+            float normalized = Mathf.Clamp(distanceFromCenter / radius, 0f, 1f);
+
+            float damage = Mathf.LerpUnclamped(minDamage, maxDamage, Curve.Evaluate(normalized));
+
+            h.ServerDamage(damage, killer, false);
+
+            HitObjects.Add(h);
+        }
+
+        HitObjects.Clear();
     }
 }
