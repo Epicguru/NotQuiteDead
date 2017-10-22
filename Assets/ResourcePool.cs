82a387c5ea1faebcba7dec1b0082ebd1d1a43583
James Billy <james@epicguru.co.uk>
2017-10-22 21:50:01 +0200

    Working on team based resource system.

---
+++ Assets/ResourcePool.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ResourcePool : NetworkBehaviour
+{
+    [SyncVar]
+    public string Team;
+
+    [SyncVar]
+    public bool Active;
+
+    public void Update()
+    {
+        if (!isServer)
+            return;
+
+        UpdateActiveState();
+    }
+
+    [Server]
+    public void UpdateActiveState()
+    {
+        Active = Teams.I.TeamExists(Team);
+    }
+}
--- Assets/Scripts/Items/MiningDrill.cs
+++ Assets/Scripts/Items/MiningDrill.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -9,25 +8,32 @@ public class MiningDrill : NetworkBehaviour
     [SyncVar]
     public bool Active;
     public Animator Animator;
+    public Vector2 DropOffest;
 
-    public List<ItemQuantityPair> ProductionPerMinute = new List<ItemQuantityPair>();
+    public List<ItemProductionRate> Production = new List<ItemProductionRate>();
 
-    private ItemPickup Pickup;
+    private ItemPickup pickup;
+    private Placeable placeable;
+    private float[] timers;
 
     public void Start()
     {
-        Pickup = GetComponent<ItemPickup>();
+        pickup = GetComponent<ItemPickup>();
+        placeable = GetComponent<Placeable>();
     }
 
     public void Update()
     {
         UpdateActiveState();
         UpdateAnimation();
+
+        if (isServer && placeable.IsPlaced && Active)
+            ProduceMaterials();
     }
 
     public void UpdateActiveState()
     {
-        if (Pickup.MouseOver && InputManager.Active)
+        if (pickup.MouseOver && InputManager.Active)
         {
             ActionHUD.DisplayAction("Press " + InputManager.GetInput("Interact") + " to " + (Active ? "turn off" : "turn on") + " mining drill.");
             if (InputManager.InputDown("Interact"))
@@ -45,13 +51,43 @@ public class MiningDrill : NetworkBehaviour
     [Server]
     public void ProduceMaterials()
     {
+        if(timers == null || timers.Length != Production.Count)
+        {
+            timers = new float[Production.Count];
+        }
 
+        for (int i = 0; i < timers.Length; i++)
+        {
+            timers[i] += Time.deltaTime;
+
+            float timer = timers[i];
+            float interval = Production[i].Interval;
+
+            while (timer >= interval)
+            {
+                timer -= interval;
+                timers[i] -= interval;
+                DropItem(Production[i].Prefab, Production[i].Count);
+            }
+        }
+    }
+
+    [Server]
+    public void DropItem(string prefab, int count)
+    {
+        Vector2 position = (Vector2)transform.position + DropOffest;
+
+        for (int i = 0; i < count; i++)
+        {
+            Player.Local.NetUtils.CmdSpawnDroppedItem(prefab, position + Random.insideUnitCircle * 0.5f, new ItemData());
+        }
     }
 }
 
-[Serializable]
-public struct ItemQuantityPair
+[System.Serializable]
+public struct ItemProductionRate
 {
     public string Prefab;
+    public float Interval;
     public int Count;
 }
