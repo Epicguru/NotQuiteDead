720bbe51b0a1a7e817cde49dafe4146927173b56
James Billy <james@epicguru.co.uk>
2017-10-23 22:10:49 +0200

    Steamworks intergration using Facepunch Steamworks!

---
+++ Assets/Scripts/Editor/Build.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+public static class Build
+{
+    public const string GameName = "NQD";
+    public const string BasePath = "Builds/";
+
+    public static string[] GetLevels()
+    {
+        return new string[]
+        {
+            "Assets/Scenes/Setup V2.unity"
+        };
+    }
+
+    [MenuItem( "Build/All" )]
+    public static void BuildAll()
+    {
+        BuildWin32();
+        BuildWin64();
+    }
+
+    [MenuItem("Build/Windows 32 bit")]
+    public static void BuildWin32()
+    {
+        BuildPlayerOptions win32 = new BuildPlayerOptions();
+        win32.locationPathName = BasePath + "Win32/" + GameName + ".exe";
+        win32.target = BuildTarget.StandaloneWindows;
+        win32.scenes = GetLevels();
+        win32.options = BuildOptions.AllowDebugging | BuildOptions.Development;
+
+        BuildPipeline.BuildPlayer(win32);
+    }
+
+    [MenuItem("Build/Windows 64 bit")]
+    public static void BuildWin64()
+    {
+        BuildPlayerOptions win64 = new BuildPlayerOptions();
+        win64.locationPathName = BasePath + "Win64/" + GameName + ".exe";
+        win64.target = BuildTarget.StandaloneWindows64;
+        win64.scenes = GetLevels();
+        win64.options = BuildOptions.AllowDebugging | BuildOptions.Development;
+
+        BuildPipeline.BuildPlayer(win64);
+    }
+}
---
+++ Assets/SteamManager.cs
@@ -0,0 +1,101 @@
+﻿using Facepunch.Steamworks;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+public class SteamManager : MonoBehaviour
+{
+    public uint AppID = 480;
+    public TextureFormat format;
+    public ServerList.Request ServerRequest;
+    public Text Text;
+    public UnityEngine.UI.Image Image;
+
+    public void Awake()
+    {
+        // Do not destroy on scene change.
+        DontDestroyOnLoad(gameObject);
+
+        // Tell unity what we are running on; Windows, Mac, Console, Potato...
+        Config.ForUnity(Application.platform.ToString());
+
+        new Client(AppID);
+
+        if(Client.Instance == null)
+        {
+            // Failed to 'start steam'. 
+            Debug.LogError("Failed to create client instance!");
+            return;
+        }       
+
+        ServerRequest = Client.Instance.ServerList.Internet();
+        Client.Instance.Achievements.Refresh();
+        Client.Instance.Friends.Refresh();
+        Client.Instance.Screenshots.Trigger();
+
+        Client.Instance.Overlay.OpenProfile(Client.Instance.SteamId);
+
+        SteamFriend first = null;
+        foreach(SteamFriend f in Client.Instance.Friends.AllFriends)
+        {
+            if (first == null || Random.value < 0.2f)
+                first = f;
+
+            Debug.Log(f.Name + ": " + (f.IsOnline ? "Online" : "Offline") + ", playing: " + f.CurrentAppId);
+        }
+
+        Debug.Log("Avatar for " + first.Name);
+        Facepunch.Steamworks.Image image = Client.Instance.Friends.GetAvatar(Friends.AvatarSize.Small, first.Id);
+        Debug.Log(image.Width + "x" + image.Height + ": " + image.IsLoaded);
+        Texture2D tex = new Texture2D(image.Width, image.Height);
+        for (int x = 0; x < image.Width; x++)
+        {
+            for (int y = 0; y < image.Height; y++)
+            {
+                var c = image.GetPixel(x, (image.Height - 1) - y);
+                tex.SetPixel(x, y, new UnityEngine.Color((float)(c.r / 255f), (float)(c.g / 255f), (float)(c.b / 255f), 1f));
+            }
+        }
+
+        tex.Apply();
+        Image.material.mainTexture = tex;
+    }
+
+    public void Update()
+    {
+        if (Client.Instance != null)
+        {
+            Client.Instance.Update();
+        }
+
+        string t = "";
+        t += "Username: " + Client.Instance.Username + "\n";
+        t += "Install folder: " + Client.Instance.InstallFolder + "\n";
+        t += "---QUERY---\n";
+        t += "Status: " + (ServerRequest.Finished ? "Finished" : "Querying") + "\n";
+        t += "Responded: " + ServerRequest.Responded.Count + "\n";
+        t += "Unresponsive: " + ServerRequest.Unresponsive.Count + "\n";
+
+        for (int i = 0; i < ServerRequest.Responded.Count; i++)
+        {
+            t += "Server #" + i + ": " + ServerRequest.Responded[i].AddressString + " - " + ServerRequest.Responded[i].Name + " - Online:" + ServerRequest.Responded[i].Players + "\n";
+        }
+
+        Text.text = t;
+    }
+
+    private void OnDestroy()
+    {
+        if (Client.Instance != null)
+        {
+            Debug.Log("Exiting steam client.");
+            Client.Instance.Dispose();
+        }
+
+        if(Server.Instance != null)
+            Server.Instance.Dispose();
+    }
+}
