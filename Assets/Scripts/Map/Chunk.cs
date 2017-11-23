05d2a0d5735ec52e1fa4a9c23afc65f1e5932fc2
James Billy <james@epicguru.co.uk>
2017-11-23 19:31:04 +0100

    Completed most texturing and debugging code for chunks.

--- Assets/Scripts/Map/Chunk.cs
+++ Assets/Scripts/Map/Chunk.cs
@@ -21,8 +21,23 @@ public class Chunk : MonoBehaviour
         InstanceCount--;
     }
 
+    public void Init()
+    {
+        // Gen mesh, then build a texture.
+        Mesh.GenMesh();
+        Texture.BuildTexture();
+    }
+
+    public void LateUpdate()
+    {
+        // Firstly, if the texture is dirty then apply it.
+        if (Texture.Dirty)
+            Texture.Apply();
+    }
+
     public bool InBounds(int x, int y)
     {
+        // Checks if a tile in within the bounds of the chunk. Local coordinates, where zero is the bottom left of the chunk.
         if (x < 0 || y < 0)
             return false;
         if (x >= Width || y >= Height)
--- Assets/Scripts/Map/MeshTexture.cs
+++ Assets/Scripts/Map/MeshTexture.cs
@@ -7,6 +7,11 @@ public class MeshTexture : MonoBehaviour
     public MeshRenderer Renderer;
     public Chunk Chunk;
 
+    public bool EnforceResolution;
+    public bool Dirty;
+
+    private Texture2D Texture;
+
     public void BuildTexture()
     {
         Texture2D texture = new Texture2D(TileResolution, TileResolution);
@@ -15,10 +20,47 @@ public class MeshTexture : MonoBehaviour
         texture.wrapMode = TextureWrapMode.Clamp;
         texture.Apply();
 
+        this.Texture = texture;
+
         Renderer.sharedMaterials[0].mainTexture = texture;
     }
 
+    public Color[] GetColours(Sprite sprite, bool packSafe = true)
+    {
+        if(sprite == null)
+        {
+            Debug.LogError("Sprite input is null, cannot extract colours!");
+            return null;
+        }
+
+        if (packSafe)
+        {
+            Rect r = sprite.textureRect;
+            return sprite.texture.GetPixels((int)r.x, (int)r.y, (int)r.width, (int)r.height);
+        }
+        else
+        {
+            return sprite.texture.GetPixels();
+        }
+    }
+
     public void SetTile(Sprite tile, int x, int y)
+    {
+        if(tile == null)
+        {
+            Debug.LogError("Null sprite input when blitting tile.");
+            return;
+        }
+
+        Color[] colours = GetColours(tile);
+
+        if (colours == null)
+            return;
+
+        SetTile(colours , x, y);
+    }
+
+    public void SetTile(Color[] colours, int x, int y)
     {
         if(!Chunk.InBounds(x, y))
         {
@@ -26,19 +68,39 @@ public class MeshTexture : MonoBehaviour
             return;
         }
 
-        Texture2D t = Renderer.sharedMaterials[0].mainTexture as Texture2D;
+        if(colours == null)
+        {
+            Debug.LogError("Null colours when blitting tile!");
+            return;
+        }
 
-        Rect r = tile.textureRect;
-        Color[] colours = tile.texture.GetPixels((int)r.x, (int)r.y, (int)r.width, (int)r.height);
+        if(EnforceResolution && colours.Length != TileResolution * TileResolution)
+        {
+            Debug.LogError("Too many pixels when blitting tile. There were " + colours.Length + " but there should have been " + TileResolution * TileResolution + ". Tile resolution should be " + TileResolution + "x" + TileResolution + ".");
+            return;
+        }
 
-        t.SetPixels(x * TileResolution, y * TileResolution, TileResolution, TileResolution, colours);
+        if (Texture == null)
+        {
+            Debug.LogError("Texture is null! Cannot write anything until texture has been built!");
+            return;
+        }
+
+        Texture.SetPixels(x * TileResolution, y * TileResolution, TileResolution, TileResolution, colours);
+        Dirty = true;
     }
 
-    public void SetTiles(Sprite tile, int x, int y, int width, int height)
+    public void SetTiles(Color[] colours, int x, int y, int width, int height)
     {
-        if(width <= 0 || height <= 0)
+        if (width <= 0 || height <= 0)
         {
             Debug.LogError("Width and height must both be greater that zero! (" + width + ", " + height + ")");
+            return;
+        }
+
+        if (colours == null)
+        {
+            Debug.LogError("Null colours when blitting tiles (multiple) !");
             return;
         }
 
@@ -46,9 +108,60 @@ public class MeshTexture : MonoBehaviour
         {
             for (int Y = y; Y < y + height; Y++)
             {
-                if(Chunk.InBounds(X, Y))
-                    SetTile(tile, X, Y);
+                if (Chunk.InBounds(X, Y))
+                    SetTile(colours, X, Y);
             }
         }
+    }
+
+    public void SetTiles(Sprite tile, int x, int y, int width, int height)
+    {
+        if(tile == null)
+        {
+            Debug.LogError("Null sprite when blitting tiles (multiple) !");
+            return;
+        }
+
+        Color[] colours = GetColours(tile);
+
+        // We know that colours is not null because the sprite is not null!
+
+        SetTiles(colours, x, y, width, height);
+    }
+
+    public void SetPixel(Color colour, int x, int y)
+    {
+        if(colour == null)
+        {
+            Debug.LogError("Null colour when blitting pixel! (" + x + ", " + y + ")");
+        }
+
+        if(Texture == null)
+        {
+            Debug.LogError("Texture is null! Cannot write anything until texture has been built!");
+            return;
+        }
+
+        Texture.SetPixel(x, y, colour);
+        Dirty = true;
+    }
+
+    public void Apply()
+    {
+        if(Texture == null)
+        {
+            Debug.LogError("Chunk texture is null, cannot apply! Must build texture so that there is something to apply!");
+            return;
+        }
+
+        if (!Dirty)
+        {
+            Debug.LogError("Chunk texture is not dirty. Cannot apply texture.");
+            return;
+        }
+
+        Texture.Apply();
+
+        Dirty = false;
     }
 }
