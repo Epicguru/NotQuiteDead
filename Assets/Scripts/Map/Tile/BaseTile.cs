6806b59bb9777d1f7a1dc44b514dedaca316591a
James Billy <james@epicguru.co.uk>
2017-12-11 19:49:11 +0100

    Cleaned up the tile script locations.

--- Assets/Scripts/Map/Tile/BaseTile.cs
+++ Assets/Scripts/Map/Tile/BaseTile.cs
@@ -1,7 +1,19 @@
 ﻿
+using UnityEngine;
+
 public abstract class BaseTile : TileBackend
 {
+    [Tooltip("The constant prefab name. Can be anything, as long as it never changes and is unique.")]
+    public string Prefab;
+
+    [Tooltip("The visual name of the tile.")]
     public string Name; // TODO add ID or Prefab
+
+    [Tooltip("Is this considered solid? Can AI walk through it?")]
+    public bool Solid = false;
+
+    [Tooltip("A reference to the physics body.")]
+    public Collider2D Physics;
 
     private TileLayer layer;
 
