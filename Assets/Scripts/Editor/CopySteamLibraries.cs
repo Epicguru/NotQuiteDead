a43a3a881c5a1d468e7f7001da9bb24781c9e581
James Billy <james@epicguru.co.uk>
2017-12-27 17:07:56 +0100

    Fixed compilation issue.

--- Assets/Scripts/Editor/CopySteamLibraries.cs
+++ Assets/Scripts/Editor/CopySteamLibraries.cs
@@ -29,7 +29,7 @@ namespace Facepunch.Editor
             if ( target == BuildTarget.StandaloneWindows64 )
                 FileUtil.ReplaceFile( "steam_api64.dll", System.IO.Path.GetDirectoryName( pathToBuiltProject ) + "/steam_api64.dll" );
 
-            if ( target == BuildTarget.StandaloneOSX )
+            if ( target == BuildTarget.StandaloneOSXUniversal )
                 FileUtil.ReplaceFile( "libsteam_api.dylib", System.IO.Path.GetDirectoryName( pathToBuiltProject ) + "/libsteam_api.dylib" );
 
             if ( target == BuildTarget.StandaloneLinux64 || target == BuildTarget.StandaloneLinuxUniversal )
