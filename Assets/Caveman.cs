d9c903bb81355c30a5a9abca90f6ee92ed965a45
James Billy <james@epicguru.co.uk>
2018-01-14 13:20:54 +0100

    Added animations for the cameman.

---
+++ Assets/Caveman.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class Caveman : NetworkBehaviour
+{
+    public Pawn Pawn;
+
+    public void Start()
+    {
+        Pawn.Health.UponDeathServer += UponDeath;
+    }
+
+    public void Update()
+    {
+        if (!isServer)
+            return;
+
+        if (Player.Local == null)
+            return;
+
+        Pawn.Path.SetTarget((int)Player.Local.transform.position.x, (int)Player.Local.transform.position.y);
+    }
+
+    [Server]
+    public void UponDeath()
+    {
+        Debug.Log("Dead!");
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Pawns/PawnAnimation.cs
@@ -0,0 +1,7 @@
+﻿
+using UnityEngine.Networking;
+
+public class PawnAnimation : NetworkBehaviour
+{
+
+}
