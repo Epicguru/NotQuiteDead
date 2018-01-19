2362db42989bfa4950a8fa165c8f87ca30364b33
James Billy <james@epicguru.co.uk>
2018-01-19 22:08:27 +0100

    Added new throwable code and animations for Frag Grenade.

--- Assets/Scripts/Weapons/Throwable/Throwable.cs
+++ Assets/Scripts/Weapons/Throwable/Throwable.cs
@@ -6,8 +6,16 @@ using UnityEngine.Networking;
 [RequireComponent(typeof(Item))]
 public class Throwable : NetworkBehaviour
 {
+    //[HideInInspector]
+    public bool DoneEquipping;
+    //[HideInInspector]
+    public bool PreparingToThrow;
+    //[HideInInspector]
+    public bool PreparedToThrow;
 
     public GameObject Prefab;
+    public Transform SpawnPostion;
+    public Animator Anim;
 
     private Item Item;
 
@@ -24,21 +32,78 @@ public class Throwable : NetworkBehaviour
         if (!Item.IsEquipped())
             return;
 
-        if (InputManager.InputDown("Shoot"))
+        // Wait until it is equipped.
+        if (!DoneEquipping)
         {
-            // Remove from holding, completely destroy...
-            GetComponentInParent<PlayerHolding>().CmdDrop(false, true, Player.Local.gameObject, new ItemData()); // Completely destroy this object...
+            return;
+        }
 
-            // Make new object.
-            CmdSpawnPrefab(Player.Local.gameObject, transform.position, Quaternion.identity, InputManager.GetMousePos());
-
-            // Look for new throwable of same type in inventory, and if we find one...
-            InventoryItem i = PlayerInventory.inv.Inventory.GetOfType(Item.Prefab);
-            if (i != null)
+        // If not preparing to throw, press Shoot to prepare the throwable.
+        if (!PreparingToThrow)
+        {
+            if (ActionEvent())
             {
-                // Equip it!
-                Item.Option_Equip(i, i.ItemPrefab);
+                PreparingToThrow = true;
+                Anim.SetTrigger("Prepare");
             }
+        }
+
+        if (PreparedToThrow)
+        {
+            // The throwable can now be thrown, but we wait until the action button has been released.
+            if (!ActionEvent())
+            {
+                // Cool, throw now!
+                Anim.SetTrigger("Throw");
+            }
+        }
+    }
+
+    private bool ActionEvent()
+    {
+        // +1 for most generic name ever.
+        return InputManager.InputPressed("Shoot");
+    }
+
+    public void Anim_CanNowThrow()
+    {
+        PreparedToThrow = true;
+    }
+
+    public void Anim_Throw()
+    {
+        if(hasAuthority)
+            SpawnInstance();
+    }
+
+    public void Anim_DoneEquipping()
+    {
+        DoneEquipping = true;
+    }
+
+    public void Anim_Done()
+    {
+        if (!hasAuthority)
+            return;
+        // Remove from holding, completely destroy...
+        GetComponentInParent<PlayerHolding>().CmdDrop(false, true, Player.Local.gameObject, new ItemData()); // Completely destroy this object...
+    }
+
+    [Client]
+    private void SpawnInstance()
+    {
+        if (SpawnPostion == null)
+            return;
+
+        // Make new object.
+        CmdSpawnPrefab(Player.Local.gameObject, SpawnPostion.position, SpawnPostion.rotation, InputManager.GetMousePos());
+
+        // Look for new throwable of same type in inventory, and if we find one...
+        InventoryItem i = PlayerInventory.inv.Inventory.GetOfType(Item.Prefab);
+        if (i != null)
+        {
+            // Equip it!
+            Item.Option_Equip(i, i.ItemPrefab);
         }
     }
 
---
+++ Assets/ThrowableAnimationCallbacks.cs
@@ -0,0 +1,34 @@
+﻿
+public class ThrowableAnimationCallbacks : ItemAnimationCallback
+{
+    private Throwable t;
+
+    // The strings are so that they don't show in the editor, [HideInInspector] does not work here.
+    private Throwable T(string a = null, string b = null)
+    {
+        if (t == null)
+            t = GetComponentInParent<Throwable>();
+
+        return t;
+    }
+
+    public void EquipEnd()
+    {
+        T().Anim_DoneEquipping();
+    }
+
+    public void ReadyToThrow()
+    {
+        T().Anim_CanNowThrow();
+    }
+
+    public void Throw()
+    {
+        T().Anim_Throw();
+    }
+
+    public void ThrowDone()
+    {
+        T().Anim_Done();
+    }
+}
