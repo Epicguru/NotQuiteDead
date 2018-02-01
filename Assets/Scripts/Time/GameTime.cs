a3d2efac31892d3d1dd5ce6a795c90b8fef5a773
James Billy <james@epicguru.co.uk>
2018-02-01 21:19:13 +0100

    Functional time system. Not implemented.

--- Assets/Scripts/Time/GameTime.cs
+++ Assets/Scripts/Time/GameTime.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+using UnityEngine;
 using UnityEngine.Networking;
 
 public class GameTime : NetworkBehaviour
@@ -13,6 +15,7 @@ public class GameTime : NetworkBehaviour
     // Value of 0.75 is afternoon day zero.
     // Value of 1.00 is midnight day one.
     [SyncVar]
+    [SerializeField]
     private float time;
 
     public void Awake()
@@ -27,7 +30,7 @@ public class GameTime : NetworkBehaviour
 
     public void Update()
     {
-        
+        DebugText.Log("Time: " + GetTimeFull());
     }
 
     public int GetDay()
@@ -48,5 +51,62 @@ public class GameTime : NetworkBehaviour
         int hours = 24;
 
         return (int)(hours * remainder);
+    }
+
+    public string GetPartOfDay()
+    {
+        int hour = GetHour();
+
+        if(hour >= 0 && hour < 6)
+        {
+            // 0 to 5
+            return "Early Morning";
+        }
+        if(hour >= 6 && hour < 11)
+        {
+            // 6 to 10
+            return "Morning";
+        }
+        if(hour >= 11 && hour < 14)
+        {
+            // 11 to 1
+            return "Midday";
+        }
+        if(hour >= 14 && hour < 19)
+        {
+            // 2 to 6
+            return "Afternoon";
+        }
+        if(hour >= 19 && hour < 22)
+        {
+            // 7 to 9
+            return "Evening";
+        }
+        if(hour >= 22 && hour < 24)
+        {
+            return "Nightime";
+        }
+
+        return "Unknown_PoD(" + hour + ")";
+    }
+
+    public string GetHourNice()
+    {
+        int hour = GetHour();
+        bool PM = hour >= 12;
+        if (PM)
+            if(hour > 12)
+                hour -= 12;
+        return hour.ToString() + (PM ? "PM" : "AM");
+    }
+
+    public string GetTime()
+    {
+        return GetHourNice();
+    }
+
+    public string GetTimeFull()
+    {
+        return "Day " + GetDay() + " " + GetPartOfDay() + " - " + GetTime();
     }
 }
