4a0821f6fdc87144edd6bd09cbbb2275bde9bc8b
James Billy <james@epicguru.co.uk>
2018-02-06 19:43:41 +0100

    Removed crap from default script.

--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -1,16 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class EnemySpawner : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class EnemySpawner : NetworkBehaviour
+{
+    
 }
