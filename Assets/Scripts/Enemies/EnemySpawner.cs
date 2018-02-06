4867b67ca8341302ce12c53722c507a7d23ea958
James Billy <james@epicguru.co.uk>
2018-02-06 20:11:03 +0100

    Added enemy spawn info.

--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -5,5 +5,45 @@ using UnityEngine.Networking;
 
 public class EnemySpawner : NetworkBehaviour
 {
-    
+    [SerializeField]
+    public List<EnemySpawningInfo> Enemies = new List<EnemySpawningInfo>();
+
+    public bool SpawnEnemies = true;
+    public float SpawnMultiplier = 1f;
+
+    public void Update()
+    {
+        if (!isServer)
+            return;
+
+        int day = GameTime.Instance.GetDay();
+        foreach (var info in Enemies)
+        {
+            // Chance per frame:
+            // CpD  * Time.deltaTime / SecondsPerDay
+
+            float enemiesPerDay = info.GetSpawnRate(day);
+        }
+    }
+
+    [Server]
+    public void Spawn(string pawn, EnemySpawnType type)
+    {
+        switch (type)
+        {
+            case EnemySpawnType.AROUND_RANDOM_PLAYER:
+
+                float minDst = 15f;
+                float maxDst = 25f;
+                float dst = Random.Range(minDst, maxDst);
+                Vector2 point = Random.insideUnitCircle.normalized;
+                point *= dst;
+
+                Vector2 position = (Vector2)Player.AllPlayers[Random.Range(0, Player.AllPlayers.Count)].transform.position + point;
+
+                Pawn.SpawnPawn(pawn, position);
+
+                break;
+        }
+    }
 }
---
+++ Assets/Scripts/Enemies/EnemySpawningInfo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class EnemySpawningInfo
+{
+    [Header("Enemy")]
+    public Pawn Pawn;
+
+    [Header("Spawn Period")]
+    public int MinDay = 0;
+    public int MaxDay = -1;
+
+    [Header("Spawn Time")]
+    public int MinHour = 21;
+    public int MaxHour = 24;
+
+    [Header("Spawn Rate")]
+    [Tooltip("The amount of enemies of this type that will spawn in one day-night cycle (not guaranteed, average). Controlled by the spawn rate curve. If MaxDay < 0, then the curve has no effect.")]
+    public float BaseSpawnRate = 10f;
+    public AnimationCurve SpawnRateCurve = AnimationCurve.Linear(0, 1, 1, 1);
+
+    [Header("Spawn Type")]
+    public EnemySpawnType SpawnType = EnemySpawnType.AROUND_RANDOM_PLAYER;
+
+    public float GetSpawnRate(int day)
+    {
+        if (day < 0)
+            return 0f;
+
+        if(MaxDay < 0)
+        {
+            if (day < MinDay)
+                return 0;
+            else
+                return BaseSpawnRate;
+        }
+        else
+        {
+            if (day < MinDay)
+                return 0;
+
+            int d = day - MinDay;
+            int range = MaxDay - MinDay;
+            float p = (float)d / (float)range;
+
+            float x = SpawnRateCurve.Evaluate(p);
+
+            float rate = BaseSpawnRate * x;
+
+            return rate;
+        }
+    }
+}
