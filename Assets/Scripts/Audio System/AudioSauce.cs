243855347abd78faf0c0b9cdf59dcc9077a8b225
James Billy <james@epicguru.co.uk>
2018-02-10 13:06:00 +0100

    Added AudioSauce class. Yes it's a joke.

---
+++ Assets/Scripts/Audio System/AudioSauce.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioSauce : MonoBehaviour
+{
+    [Header("Basics")]
+    public AudioClip Clip;
+    public float Pitch = 1f;
+    [Range(0f, 1f)]
+    public float Volume = 1f;
+
+    [Header("Falloff")]
+    [Range(1f, 1000f)]
+    public float Range = 100f;
+    public AnimationCurve SoundFalloff = AnimationCurve.Linear(0, 1, 1, 0);
+
+    public AudioSource Source { get; private set; }
+
+    public void Awake()
+    {
+        Source = GetComponent<AudioSource>();
+
+        if(Source == null)
+        {
+            Debug.LogError("Audio Sauce's AudioSource component is null!");
+            return;
+        }
+
+        ConfigureSource(Source);
+    }
+
+    public virtual void ConfigureSource(AudioSource source)
+    {
+        source.spatialBlend = 0f;
+    }
+}
