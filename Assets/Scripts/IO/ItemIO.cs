8fef96e10a01489691bf522f3a2c823b62d7c9d6
James Billy <james@epicguru.co.uk>
2018-02-20 19:41:45 +0100

    Added ItemIO static class.

---
+++ Assets/Scripts/IO/ItemIO.cs
@@ -0,0 +1,13 @@
+﻿
+public static class ItemIO
+{
+    public static string ItemToString(Item i)
+    {
+        if (i == null)
+            return null;
+
+        string prefab = i.Prefab;
+
+        return null;
+    }
+}
