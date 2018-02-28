a99ff5f9a1dcfe083bccd5c1784dd2c2a35d19f8
James Billy <james@epicguru.co.uk>
2018-02-28 22:12:40 +0100

    Added development item data class called ItemDataX.

---
+++ Assets/ItemDataXTest.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDataXTest : MonoBehaviour
+{
+    public ItemDataX ItemData;
+
+    public void Start()
+    {
+        ItemData = new ItemDataX();
+        ItemData.Created = true;
+        ItemData.Add("XYZ", 123);
+        ItemData.Add("Vector3", new Vector3(1f, 2f, 3f));
+        ItemData.Add("James", "A cool person, yea!");
+        ItemData.Add("Array Name Here", new float[] { 10f, 20f, 50f, 100f, 0.01f });
+        //ItemData.Add("A colour!", new Color(0.5f, 0.5f, 1f, 1f));
+
+        JsonSerializerSettings settings = new JsonSerializerSettings();
+
+        settings.Formatting = Formatting.Indented;
+        settings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+
+        Debug.Log(JsonConvert.SerializeObject(ItemData, settings));
+    }
+}
---
+++ Assets/Scripts/Item System/New Item Data/ItemDataX.cs
@@ -0,0 +1,97 @@
+﻿
+using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+[JsonObject(MemberSerialization = MemberSerialization.OptOut)]
+public class ItemDataX
+{
+    [ReadOnly]
+    public bool Created = false;
+
+    [JsonProperty]
+    private Dictionary<string, object> values = new Dictionary<string, object>();
+
+    public void Add(string key, object value)
+    {
+        if (values.ContainsKey(key))
+        {
+            Debug.LogError("This item data already contains the key '" + key + "'!");
+            return;
+        }
+        else
+        {
+            values.Add(key, value);
+        }
+    }
+
+    public void Update(string key, object value, bool createIfMissing = true)
+    {
+        if (!values.ContainsKey(key))
+        {
+            if (createIfMissing)
+            {
+                Add(key, value);
+                return;
+            }
+            else
+            {
+                Debug.LogError("Cannot update value for key '" + key + "', the key is not present!");
+                return;
+            }
+        }
+        else
+        {
+            values[key] = value;
+        }
+    }
+
+    public bool ContainsKey(string key)
+    {
+        return values.ContainsKey(key);
+    }
+
+    public T Get<T>(string key)
+    {
+        if (ContainsKey(key))
+        {
+            try
+            {
+                T x = (T)values[key];
+                return x;
+            }
+            catch (InvalidCastException e)
+            {
+                Debug.LogError("Invalid cast in item data for key '" + key + "': Value is of type '" + values[key].GetType().ToString() + "', requested type was '" + typeof(T).ToString() + "'!\n" + e);
+                return default(T);
+            }
+        }
+        else
+        {
+            return default(T);
+        }
+    }
+
+    public T Get<T>(string key, T defaultValue)
+    {
+        if (ContainsKey(key))
+        {
+            try
+            {
+                T x = (T)values[key];
+                return x;
+            }
+            catch (InvalidCastException e)
+            {
+                Debug.LogError("Invalid cast in item data for key '" + key + "': Value is of type '" + values[key].GetType().ToString() + "', requested type was '" + typeof(T).ToString() + "'!\n" + e);
+                return defaultValue;
+            }
+        }
+        else
+        {
+            return defaultValue;
+        }
+    }
+}
