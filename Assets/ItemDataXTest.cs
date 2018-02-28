1c20f74abc1c1d854cb33554ee3365f36145a14f
James Billy <james@epicguru.co.uk>
2018-02-28 22:24:15 +0100

    Added custom unity resolver to remove data redundancy with Unity types.

--- Assets/ItemDataXTest.cs
+++ Assets/ItemDataXTest.cs
@@ -15,12 +15,13 @@ public class ItemDataXTest : MonoBehaviour
         ItemData.Add("Vector3", new Vector3(1f, 2f, 3f));
         ItemData.Add("James", "A cool person, yea!");
         ItemData.Add("Array Name Here", new float[] { 10f, 20f, 50f, 100f, 0.01f });
-        //ItemData.Add("A colour!", new Color(0.5f, 0.5f, 1f, 1f));
+        ItemData.Add("A colour!", new Color(0.5f, 0.5f, 1f, 1f));
 
         JsonSerializerSettings settings = new JsonSerializerSettings();
 
         settings.Formatting = Formatting.Indented;
         settings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+        settings.ContractResolver = UnityContractResolver.Instance;
 
         Debug.Log(JsonConvert.SerializeObject(ItemData, settings));
     }
---
+++ Assets/Scripts/Item System/New Item Data/UnityContractResolver.cs
@@ -0,0 +1,35 @@
+﻿
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Reflection;
+using UnityEngine;
+
+public class UnityContractResolver : DefaultContractResolver
+{
+    public static readonly UnityContractResolver Instance = new UnityContractResolver();
+
+
+    protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+    {
+        JsonProperty property = base.CreateProperty(member, memberSerialization);
+
+        // Vector 2, 3, and 4
+        if (property.DeclaringType == typeof(Vector2) || property.DeclaringType == typeof(Vector3) || property.DeclaringType == typeof(Vector4))
+        {
+            if(property.PropertyName == "normalized" || property.PropertyName == "magnitude" || property.PropertyName == "sqrMagnitude")
+            {
+                property.Ignored = true;
+            }
+        }
+
+        // Colour
+        if (property.DeclaringType == typeof(Color))
+        {
+            if (property.PropertyName == "linear" || property.PropertyName == "gamma" || property.PropertyName == "grayscale" || property.PropertyName == "maxColorComponent")
+            {
+                property.Ignored = true;
+            }
+        }
+        return property;
+    }
+}
