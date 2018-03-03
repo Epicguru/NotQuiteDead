4730ea5a69e5a3137df3ea4806461aaef4a8a829
James Billy <james@epicguru.co.uk>
2018-03-03 13:58:48 +0100

    Slightly improved credits screen.

--- Assets/Scripts/UI/Credits.cs
+++ Assets/Scripts/UI/Credits.cs
@@ -4,8 +4,12 @@ using UnityEngine.UI;
 
 public class Credits : MonoBehaviour
 {
+    public RectTransform Canvas;
     public ScrollRect SV;
     public Text Text;
+
+    public Vector2 VerticalRotation;
+    public Vector2 HorizontalRotation;
 
     public void Start()
     {
@@ -25,5 +29,13 @@ public class Credits : MonoBehaviour
         {
             SceneManager.LoadScene("Main Menu");
         }
+
+        float x = Input.mousePosition.x / Screen.width;
+        float y = Input.mousePosition.y / Screen.height;
+
+        float rotY = Mathf.Lerp(HorizontalRotation.x, HorizontalRotation.y, x);
+        float rotX = Mathf.Lerp(VerticalRotation.x, VerticalRotation.y, y);
+
+        Canvas.rotation = Quaternion.Euler(new Vector3(rotX, rotY, 0f));
     }
 }
