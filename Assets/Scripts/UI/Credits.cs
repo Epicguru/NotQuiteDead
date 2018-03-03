1dfe5e7b749d6205feb3b39181a860f9c0f4210d
James Billy <james@epicguru.co.uk>
2018-03-03 13:46:23 +0100

    Added crappy credits screen.

---
+++ Assets/Scripts/UI/Credits.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Credits : MonoBehaviour
+{
+    public ScrollRect SV;
+    public Text Text;
+
+    public void Start()
+    {
+        SetText();
+    }
+
+    public void SetText()
+    {
+        Text.text = Resources.Load<TextAsset>("Credits").text;
+    }
+
+    public void Update()
+    {
+        SV.verticalNormalizedPosition -= 0.1f * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Main Menu");
+        }
+    }
+}
