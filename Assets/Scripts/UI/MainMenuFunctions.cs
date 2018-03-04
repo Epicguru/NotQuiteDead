9c83be6515c12b64da7a197b3a0e143688f7f287
James Billy <james@epicguru.co.uk>
2018-03-04 13:53:48 +0100

    UI keeps on messing up :(

--- Assets/Scripts/UI/MainMenuFunctions.cs
+++ Assets/Scripts/UI/MainMenuFunctions.cs
@@ -1,15 +1,46 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 
 public class MainMenuFunctions : MonoBehaviour
 {
+    public MainMenuTransitions T;
+    public LoadingText LT;
+    public AsyncOperation AO;
+
     public void OpenCredits()
     {
+        if (T.IsLoading)
+            return;
         SceneManager.LoadScene("Credits", LoadSceneMode.Single);
     }
 
     public void LoadReality()
     {
-        SceneManager.LoadScene("Setup V2", LoadSceneMode.Single);
+        T.IsLoading = true;
+        LT.RealityName = "Devland";
+        LT.RealityDay = 123;
+        AsyncOperation ao = SceneManager.LoadSceneAsync("Setup V2", LoadSceneMode.Single);
+        ao.completed += LoadRealitySceneLoaded;
+        AO = ao;
+    }
+
+    private void LoadRealitySceneLoaded(AsyncOperation ao)
+    {
+        Debug.Log("Scene loaded, starting host...");
+        GameObject.Find("Network Manager").GetComponent<NetworkManager>().StartHost();
+        AO = null;
+    }
+
+    public void Update()
+    {
+        if (T.IsLoading)
+        {
+            if(AO != null)
+            {
+                LT.Percentage = AO.progress * 100f;
+                Debug.Log(AO.progress);
+            }
+        }
     }
 }
---
+++ Assets/Scripts/UI/MainMenuTransitions.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuTransitions : MonoBehaviour
+{
+    public RectTransform Menu, Loading;
+
+    public bool IsLoading;
+    public float TransitionTime = 1f;
+    public AnimationCurve TransitionCurve;
+
+    private float timer;
+
+    public void Update()
+    {
+        // Update timers and stuff.
+        UpdateTimer();
+
+        // Calculate menu y pos;
+        float y = GetProgress() * Screen.height;
+        Menu.anchoredPosition = new Vector2(0f, y);
+
+        y = GetProgress() * Screen.height - Screen.height;
+        Loading.anchoredPosition = new Vector2(0f, y);
+    }
+
+    public void UpdateTimer()
+    {
+        timer += Time.unscaledDeltaTime * (IsLoading ? 1f : -1f);
+        timer = Mathf.Clamp(timer, 0f, TransitionTime);
+    }
+
+    public float GetProgress()
+    {
+        float p = Mathf.Clamp(timer / TransitionTime, 0f, 1f);
+        float x = TransitionCurve.Evaluate(p);
+        return x;
+    }
+}
