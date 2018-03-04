b7721200afea6fc6086b485d2d9ca19c931d0236
James Billy <james@epicguru.co.uk>
2018-03-04 10:32:04 +0100

    Added basic main menu functions.

---
+++ Assets/Scripts/UI/MainMenuFunctions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuFunctions : MonoBehaviour
+{
+    public void OpenCredits()
+    {
+        SceneManager.LoadScene("Credits", LoadSceneMode.Single);
+    }
+
+    public void LoadReality()
+    {
+        SceneManager.LoadScene("Setup V2", LoadSceneMode.Single);
+    }
+}
