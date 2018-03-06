e6dd2f56bec83a6a50e2400f5bdfcee2f5244ba1
James Billy <james@epicguru.co.uk>
2018-03-06 19:31:52 +0100

    Adding background for main menu.

---
+++ Assets/Scripts/UI/MainMenuImage.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+
+public class MainMenuImage : MonoBehaviour
+{
+    public Vector3 RandomRot;
+    public Vector2 Interval;
+    public float Magnitude;
+    public float ChangeInterval;
+    public Animator Anim;
+    public Sprite[] Sprites;
+
+    private float angleTimer;
+    private float changeTimer;
+    private float currentInterval;
+    private Vector3 oldAngle;
+    private int spriteIndex;
+    private SpriteRenderer sprRenderer;
+
+    public void Start()
+    {
+        sprRenderer = GetComponent<SpriteRenderer>();
+        oldAngle = transform.localEulerAngles;
+        spriteIndex = RandomSpriteIndex();
+        SetNewSprite();
+    }
+
+    public void Update()
+    {
+        // Rotation.
+        angleTimer += Time.unscaledDeltaTime;
+        if(angleTimer > currentInterval)
+        {
+            oldAngle = RandomRot;
+            NewRandomRotation();
+            angleTimer -= currentInterval;
+            currentInterval = Random.Range(Interval.x, Interval.y);
+        }
+
+        LerpToTarget();
+
+        // Transition.
+        changeTimer += Time.unscaledDeltaTime;
+        if(changeTimer > ChangeInterval)
+        {
+            changeTimer -= ChangeInterval;
+
+            int newIndex;
+            do
+            {
+                newIndex = RandomSpriteIndex();
+
+            } while (newIndex == spriteIndex);
+
+            spriteIndex = newIndex;
+
+            // Trigger animation...
+            Anim.SetTrigger("Transition");
+
+            // The sprite from the newly selected index is applied during the animation using a callback.
+        }
+    }
+
+    public void SetNewSprite()
+    {
+        sprRenderer.sprite = Sprites[spriteIndex];
+    }
+
+    public int RandomSpriteIndex()
+    {
+        return Random.Range(0, Sprites.Length);
+    }
+
+    public void NewRandomRotation()
+    {
+        float x = Random.Range(-Magnitude, Magnitude) * 0.4f;
+        float y = Random.Range(-Magnitude, Magnitude);
+        float z = Random.Range(-Magnitude, Magnitude);
+        RandomRot = new Vector3(x, y, z);
+    }
+
+    private void LerpToTarget()
+    {
+        Vector3 newRot = LerpAngle(oldAngle, RandomRot, angleTimer / currentInterval);
+
+        transform.localEulerAngles = newRot;
+    }
+
+    private Vector3 LerpAngle(Vector3 a, Vector3 b, float t)
+    {
+        float x = Mathf.LerpAngle(a.x, b.x, t);
+        float y = Mathf.LerpAngle(a.y, b.y, t);
+        float z = Mathf.LerpAngle(a.z, b.z, t);
+
+        return new Vector3(x, y, z);
+    }
+}
