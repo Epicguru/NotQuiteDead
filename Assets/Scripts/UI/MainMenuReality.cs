6ab6549880ad083cf3bbcf940cf6b22cff5ce7aa
James Billy <james@epicguru.co.uk>
2018-03-07 19:12:03 +0100

    Added a few classes to manage the new reality UI.

---
+++ Assets/Scripts/UI/MainMenuReality.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuReality : MonoBehaviour
+{
+    [Header("Header")]
+    public Text NameText;
+    public Image IconImage;
+
+    [Header("Runtime")]
+    public string RealityName;
+    public Sprite Icon;
+
+    public void Start()
+    {
+        UpdateVisuals();
+    }
+
+    public void UpdateVisuals()
+    {
+        NameText.text = string.IsNullOrWhiteSpace(RealityName) ? "(null)" : RealityName.Trim();
+        IconImage.sprite = Icon;
+    }
+
+    public void Clicked()
+    {
+        Debug.Log("Clicked on '" + RealityName + "'");
+    }
+}
--- Assets/Scripts/UI/RealitySelectScreen.cs
+++ Assets/Scripts/UI/RealitySelectScreen.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class RealitySelectScreen : MonoBehaviour
 {
+    [Header("References")]
     public MainMenuTransitions T;
 
     public void Update()
