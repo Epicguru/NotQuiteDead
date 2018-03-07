6e3b5eb3c46febedaf2b99075cf914fb9f7b2c3f
James Billy <james@epicguru.co.uk>
2018-03-07 21:00:25 +0100

    Added cross scene reality loader.

---
+++ Assets/Scripts/Utils/MainMenuRealityLoader.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuRealityLoader : MonoBehaviour
+{
+    public string RealityName;
+    public bool Host;
+
+    public void Start ()
+	{
+        DontDestroyOnLoad(this.gameObject);
+        Debug.Log("Start, current scene is " + SceneManager.GetActiveScene().name);
+	}
+
+    public void SceneChange()
+    {
+        Debug.Log("Scene change, scene is now " + SceneManager.GetActiveScene().name);
+    }
+	
+	public void Update ()
+	{
+		
+	}
+}
