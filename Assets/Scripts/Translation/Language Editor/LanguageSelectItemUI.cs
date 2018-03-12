642bd07b1f7ae730b48df7e8fd203b2fb3cef8da
James Billy <james@epicguru.co.uk>
2018-03-12 18:57:48 +0100

    Adding UI and code for a language creation menu.

---
+++ Assets/Scripts/Translation/Language Editor/LanguageSelectItemUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LanguageSelectItemUI : MonoBehaviour
+{
+    public string Language;
+
+    public Text Text;
+
+	public void Start ()
+	{
+        Text.text = Language.Trim();
+	}
+	
+	public void Selected()
+    {
+        Debug.Log("Clicked on language " + Language);
+    }
+}
---
+++ Assets/Scripts/Translation/Language Editor/LanguageSelectUI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class LanguageSelectUI : MonoBehaviour
+{
+    public RectTransform Content;
+    public LanguageSelectItemUI Prefab;
+
+    public List<string> Langs;
+
+    private List<GameObject> spawned = new List<GameObject>();
+
+    public void Start()
+    {
+        SpawnAll(Langs.ToArray());
+    }
+
+    public void DestroySpawned()
+    {
+        foreach (var s in spawned)
+        {
+            Destroy(s);
+        }
+    }
+
+    public void SpawnAll(params string[] languages)
+    {
+        DestroySpawned();
+
+        foreach (var lang in languages)
+        {
+            var spawn = Instantiate(Prefab, Content);
+            spawn.Language = lang;
+            spawned.Add(spawn.gameObject);
+        }
+    }
+}
