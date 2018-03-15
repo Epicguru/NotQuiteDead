d5e1ae742550b0dbb86d63bea1f353a78875d2e0
James Billy <james@epicguru.co.uk>
2018-03-15 11:17:38 +0100

    Added some code to the new class.

--- Assets/Scripts/Translation/Language Editor/LanguageDefinitionItemUI.cs
+++ Assets/Scripts/Translation/Language Editor/LanguageDefinitionItemUI.cs
@@ -1,14 +1,56 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class LanguageDefinitionItemUI : MonoBehaviour
 {
-	public void Start ()
-	{
-		
-	}
-	
-	public void Update ()
-	{
-		
-	}
+    public Image BackgroundImage;
+    public Image LockedImage;
+    public Toggle Lock;
+    public InputField NameInput;
+    public InputField DescriptionInput;
+    public InputField ParamsInput;
+    public Text ErrorText;
+    public Color NormalColour;
+    public Color ErrorColour;
+
+    public void DeleteButtonPressed()
+    {
+        Destroy(this.gameObject);
+    }
+
+    public void Update()
+    {
+        LockedImage.enabled = Lock.isOn;
+
+        UpdateError();
+    }
+
+    public void UpdateError()
+    {
+        string error = GetErrors();
+        bool isError = error != null;
+
+        if (isError)
+        {
+            ErrorText.text = error;
+            BackgroundImage.color = ErrorColour;
+        }
+        else
+        {
+            ErrorText.text = "";
+            BackgroundImage.color = NormalColour;
+        }
+    }
+
+    public string GetErrors()
+    {
+        // Check for invalid name:
+        if (string.IsNullOrWhiteSpace(NameInput.text.Trim()))
+        {
+            return "Name is empty!";
+        }
+        // TODO check for duplicate name.
+
+        return null;
+    }
 }
