5cc243736d6b871b11477c5e864b62fbcd04e8f1
James Billy <james@epicguru.co.uk>
2018-03-29 22:15:24 +0200

    Fixed main menu credits UI issue.

---
+++ Assets/Scripts/UI/MainMenuCreditsScrollPos.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuCreditsScrollPos : MonoBehaviour
+{
+    public ScrollRect SV;
+
+    public void OnEnable()
+    {
+        SV.verticalNormalizedPosition = 1f;
+    }
+}
