2e399797a90edd9aa25a709a3ee97720bbc3458e
James Billy <james@epicguru.co.uk>
2018-03-30 18:35:38 +0200

    Re-doing the inventory UI.

---
+++ Assets/Scripts/Item System/Inventories/Inventory UI/InventoryItemUI.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryItemUI : MonoBehaviour
+{
+    [Header("References")]
+    public Text NameText;
+    public Image IconImage;
+
+    [Header("Controls")]
+    public string Name;
+    public Sprite Icon;
+    public int Count;
+
+    [Header("Data")]
+    public string Prefab;
+
+    public void Start()
+    {
+        UpdateVisuals();
+    }
+
+    public void UpdateVisuals()
+    {
+        NameText.text = Name.Trim() + (Count > 1 ? " x" + Count : "");
+        IconImage.sprite = Icon;
+    }
+
+    public void Clicked()
+    {
+        Debug.Log("Clicked '{0}'".Form("Prefab"));
+    }
+}
---
+++ Assets/Scripts/Item System/Inventories/Inventory UI/InventoryUI.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    public Inventory Inventory
+    {
+        get
+        {
+            return inventory;
+        }
+        set
+        {
+            if (value == inventory)
+                return;
+
+            inventory = value;
+            InventoryChanged(inventory);
+        }
+    }
+    [Header("Data")]
+    [SerializeField]
+    private Inventory inventory;
+
+    [Header("References")]
+    public RectTransform ContentParent;
+    public InventoryItemUI Prefab;
+
+    private List<InventoryItemUI> spawned = new List<InventoryItemUI>();
+
+    private void InventoryChanged(Inventory newInventory)
+    {
+        DestroySpawned();
+
+        if(newInventory != null)
+        {
+            SpawnItems(newInventory);
+        }
+    }
+
+    public void SpawnItems(Inventory inv)
+    {
+        if (inv == null)
+            return;
+
+        foreach (var pair in inv.Content)
+        {
+            Item itemPrefab = Item.GetItem(pair.Key);
+
+            if (itemPrefab == null)
+            {
+                Debug.LogError("Cannot spawn '{0}' as an inventory UI item! Item prefab not found!".Form(pair.Key));
+                continue;
+            }
+
+            // Spawn based on the stackablilty of the item.
+            if (itemPrefab.CanStack)
+            {
+                ItemStack stack = pair.Value[0];
+
+                // Spawn the item stack...
+                var invItem = Instantiate(Prefab, ContentParent);
+                invItem.Prefab = stack.Prefab;
+                invItem.Count = stack.Count;
+                invItem.Name = itemPrefab.Name;
+
+                spawned.Add(invItem);
+            }
+            else
+            {
+                for (int i = 0; i < pair.Value.Count; i++)
+                {
+                    ItemStack stack = pair.Value[i];
+
+                    // Spawn the item...
+                    var invItem = Instantiate(Prefab, ContentParent);
+                    invItem.Prefab = stack.Prefab;
+                    invItem.Name = itemPrefab.Name;
+                    invItem.Count = 1; // Always one.
+
+                    spawned.Add(invItem);
+                }
+            }
+        }
+    }
+
+    public void DestroySpawned()
+    {
+        foreach (var go in spawned)
+        {
+            if(go != null)
+            {
+                Destroy(go);
+            }
+        }
+        spawned.Clear();
+    }
+}
