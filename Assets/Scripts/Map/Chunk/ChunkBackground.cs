22974ccdf344aa6c476c568445b4bdbd2fd1e545
James Billy <james@epicguru.co.uk>
2018-04-06 09:43:36 +0200

    Working on smooth chunk background blending.

---
+++ Assets/Scripts/Map/Chunk/ChunkBackground.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class ChunkBackground : MonoBehaviour
+{
+    [Header("Controls")]
+    public int ChunkSize = 16;
+    public Sprite CurrentBG;
+    public Sprite BlendedBG;
+    public Sprite Mask;
+
+    [Header("References")]
+    public Transform Scale;
+    public SpriteRenderer Renderer;
+
+    public void Start()
+    {
+        Apply();
+    }
+
+    public void Apply()
+    {
+        Scale.localScale = Vector3.one * ChunkSize;
+        Renderer.material.SetFloat("_Tiling", ChunkSize);
+        Renderer.material.SetTexture("_OtherTex", BlendedBG.texture);
+        Renderer.material.SetTexture("_MaskTex", Mask.texture);
+        Renderer.sprite = CurrentBG;
+    }
+}
