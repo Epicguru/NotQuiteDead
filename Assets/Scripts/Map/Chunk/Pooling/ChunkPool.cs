6f199be76ac41cede4e366d27b81a1bb5f1ce4b5
James Billy <james@epicguru.co.uk>
2018-04-09 18:25:21 +0200

    Added core chunk pooling functions.

--- Assets/Scripts/Map/Chunk/Pooling/ChunkPool.cs
+++ Assets/Scripts/Map/Chunk/Pooling/ChunkPool.cs
@@ -1,14 +1,62 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class ChunkPool : MonoBehaviour
 {
-	public void Start()
-	{
-		
-	}
-	
-	public void Update()
-	{
-		
-	}
+    public static ChunkPool Instance;
+    public GameObject Prefab;
+
+    public int Max = 100;
+
+    [ReadOnly]
+    public int Pooled;
+    [ReadOnly]
+    public int OnLease;
+    [ReadOnly]
+    public int Total;
+
+    private List<Chunk> Spawned = new List<Chunk>();
+
+    public void Awake()
+    {
+        Instance = this;
+    }
+
+    public void OnDestroy()
+    {
+        Spawned.Clear();
+        Spawned = null;
+        Instance = null;
+    }
+
+    public void DestroyPooled()
+    {
+        foreach (var chunk in Spawned)
+        {
+            Destroy(chunk);
+        }
+        Spawned.Clear();
+    }
+
+    private Chunk SpawnNewChunk()
+    {
+        return Instantiate(Prefab).GetComponent<Chunk>();
+    }
+
+    public Chunk GetChunk()
+    {
+        if(Spawned.Count > 0)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+    public void ReturnChunk()
+    {
+
+    }
 }
