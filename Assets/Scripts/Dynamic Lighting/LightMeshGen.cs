3c6fb8bcd0088603e3ffdd9c2c085e88456ff064
James Billy <james@epicguru.co.uk>
2018-04-09 09:57:54 +0200

    Adding vertex colour shader and scripts. Removed unused mat.

---
+++ Assets/Scripts/Dynamic Lighting/LightMeshGen.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class LightMeshGen : MonoBehaviour
+{
+    public MeshFilter Filter;
+
+    public int Width;
+    public int Height;
+    public float Size = 1;
+
+    public void GenMesh()
+    {
+        int numTiles = Width * Height;
+        int numTris = numTiles * 2;
+
+        int vsize_x = Width + 1;
+        int vsize_z = Height + 1;
+        int numVerts = vsize_x * vsize_z;
+
+        // Generate the mesh data
+        Vector3[] vertices = new Vector3[numVerts];
+        Vector3[] normals = new Vector3[numVerts];
+        Vector2[] uv = new Vector2[numVerts];
+
+        int[] triangles = new int[numTris * 3];
+
+        int x, y;
+        for (y = 0; y < vsize_z; y++)
+        {
+            for (x = 0; x < vsize_x; x++)
+            {
+                vertices[y * vsize_x + x] = new Vector3(x * Size, y * Size, 0);
+                normals[y * vsize_x + x] = -Vector3.forward;
+                uv[y * vsize_x + x] = new Vector2((float)x / Width, 1f - (float)y / Height);
+            }
+        }
+
+        for (y = 0; y < Height; y++)
+        {
+            for (x = 0; x < Width; x++)
+            {
+                int squareIndex = y * Width + x;
+                int triOffset = squareIndex * 6;
+                triangles[triOffset + 1] = y * vsize_x + x + 0;
+                triangles[triOffset + 2] = y * vsize_x + x + vsize_x + 0;
+                triangles[triOffset + 0] = y * vsize_x + x + vsize_x + 1;
+
+                triangles[triOffset + 4] = y * vsize_x + x + 0;
+                triangles[triOffset + 5] = y * vsize_x + x + vsize_x + 1;
+                triangles[triOffset + 3] = y * vsize_x + x + 1;
+            }
+        }
+
+        // Create a new Mesh and populate with the data
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.normals = normals;
+        mesh.uv = uv;
+
+        Filter.mesh = mesh;
+    }
+}
---
+++ Assets/Shaders/Vertex Colour/Vertex Colour.shader
@@ -0,0 +1,49 @@
+﻿Shader "Hidden/Vertex Colour"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		// No culling or depth
+		Cull Off ZWrite Off ZTest Always
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			// vertex input: position, color
+			struct appdata {
+				float4 vertex : POSITION;
+				fixed4 color : COLOR;
+			};
+
+			struct v2f {
+				float4 pos : SV_POSITION;
+				fixed4 color : COLOR;
+			};
+        
+			v2f vert (appdata v) {
+				v2f o;
+				o.pos = UnityObjectToClipPos(v.vertex );
+				o.color = v.color;
+				return o;
+			}
+			
+			sampler2D _MainTex;
+
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed4 col = i.color;
+				
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
